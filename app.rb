a9f274f11577e363a579da560f9b21744a1f0d39
Stacey Astoveza <staceyastoveza@transfix.io>
2017-08-08 17:02:47 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do 
+		erb :user_input
+	end
+
+	post '/piglatinize' do 
+		@string = params[:user_phrase]
+		@piglatin = PigLatinizer.new
+
+		erb :results
+	end
+
+
+
 end
